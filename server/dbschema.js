17368fa3553e015cda280a7c7b3897a3819959a8
faisal.mohamoud <Faisal.Mohamoud89@seavus.com>
2019-09-12 14:07:32 +0200

    Setup api routes for Adding and Getting jokes

--- server/dbschema.js
+++ server/dbschema.js
@@ -2,17 +2,17 @@ const db = {
     jokes: [
         {
             username: 'user1',
-            body: 'Hear about the new restaurant called Karma? There’s no menu: You get what you deserve.',
-            createdAt: '2019-09-12T10:22:48.829Z',
-            likeCount: 5,
-            commentCount: 3
+            body: 'Did you hear about the crook who stole a calendar? He got twelve months.',
+            createdAt: '2019-09-12T11:59:58.903Z',
+            likeCount: 11,
+            commentCount: 7
         },
         {
             username: 'user2',
-            body: 'Did you hear about the crook who stole a calendar? He got twelve months.',
-            createdAt: '2019-09-11T18:17:48.480Z',
-            likeCount: 11,
-            commentCount: 7
-        }
+            body: 'Hear about the new restaurant called Karma? There’s no menu: You get what you deserve.',
+            createdAt: '2019-09-12T12:02:45.148Z',
+            likeCount: 5,
+            commentCount: 3
+        },
     ]
 }
--- server/functions/index.js
+++ server/functions/index.js
@@ -8,7 +8,43 @@ const app = express();
 
 const db = admin.firestore();
 
+// Set api routes
 
+app.get('/jokes', (req, res)=> {
+  db.collection('jokes')
+    .get()
+    .then(snapshots => {
+      const jokes = [];
+        snapshots.forEach(doc => {
+          jokes.push({
+            jokeId: doc.id,
+            ...doc.data()
+          });
+        });
+      return res.json(jokes);
+    })
+    .catch(err => {
+      res.json({error: err});
+      console.error('== ERROR ==', err);
+    });
+});
+
+
+app.post('/joke', (req, res)=>{
+  const newJoke = {
+    username: req.body.username, 
+    body: req.body.body,
+    createdAt: new Date().toISOString()
+  };
+
+  db.collection('jokes')
+    .add(newJoke)
+    .then(doc => res.json({message: `document ${doc.id} created successfully`}))
+    .catch(err => {
+      res.status(500).json({error: 'something went wrong'});
+      console.error('== ERROR ==', err);
+    });
+});
 
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
