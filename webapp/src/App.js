1b873a88771d8f1027730aead04d3d616ffa7514
faisal.mohamoud <Faisal.Mohamoud89@seavus.com>
2019-09-28 17:03:00 +0200

    Setup app web-client with create-react-app

---
+++ webapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="main-wrapper">
+     <h1>Hello world from comeesy!</h1>
+    </div>
+  );
+}
+
+export default App;
