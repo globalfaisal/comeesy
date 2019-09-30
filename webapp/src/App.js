8f9e7e31998362fec589fce716effbb7bc4ceef9
faisal.mohamoud <Faisal.Mohamoud89@seavus.com>
2019-09-30 11:27:54 +0200

    Revert "Eject create-react-app and configure eslint-wesbos"
    This reverts commit a8ae983cf89e0c9f8261de05afb57dd37d0e1478.

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
-const App = () => (
-  <div className="main-wrapper">
-    <h1>Hello world from comeesy!</h1>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="main-wrapper">
+     <h1>Hello world from comeesy!</h1>
+    </div>
+  );
+}
 
 export default App;
